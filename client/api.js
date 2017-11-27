ddd9f6e4a991484ad42305082d7c79b12bf19fa2
Shmuel Lotman <shmuel.lotman@gmail.com>
2017-11-27 16:17:08 -0500

    Final
    Final

---
+++ client/api.js
@@ -0,0 +1,12 @@
+const fetchAttractions = () =>
+  fetch("/api")
+    .then(result => result.json())
+    .catch(err => console.error(err));
+
+const fetchItinerary = (hash) => 
+  fetch('/api/itineraries/' + hash)
+  .then(result => result.json())
+module.exports = {
+  fetchAttractions,
+  fetchItinerary
+};
---
+++ server/models/index.js
@@ -0,0 +1,19 @@
+const db = require('./_db');
+
+const Place = require('./place');
+const Hotel = require('./hotel');
+const Restaurant = require('./restaurant');
+const Activity = require('./activity');
+const Itinerary = require('./itinerary');
+Hotel.belongsTo(Place);
+Restaurant.belongsTo(Place);
+Activity.belongsTo(Place);
+
+module.exports = {
+	db,
+	Place,
+	Hotel,
+	Restaurant,
+	Activity,
+	Itinerary
+};
